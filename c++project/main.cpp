ad7b452754a94d78b23592ee284d08b4bb383430
Mara Kim <hacker.root@gmail.com>
2013-09-05 17:16:50 -0500

    removed unnecessary operators

--- c++project/main.cpp
+++ c++project/main.cpp
@@ -31,10 +31,10 @@ void printhelp()
     // output help
     printshorthelp();
     std::cout << "<Description>\n"
-              << "Option\t\tGNU long option\t\tMeaning\n"
-              << "-h, -?\t\t--help\t\t\tShow this message\n"
-              << "-v\t\t--version\t\tOutput program version\n"
-              << "-V\t\t--version-long\t\tOutput full program version\n";
+                 "Option\t\tGNU long option\t\tMeaning\n"
+                 "-h, -?\t\t--help\t\t\tShow this message\n"
+                 "-v\t\t--version\t\tOutput program version\n"
+                 "-V\t\t--version-long\t\tOutput full program version\n";
 }
 
 void printversion()
