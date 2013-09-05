2e039eba3611a0c537cc63fb3c993170f2b2f07a
Mara Kim <hacker.root@gmail.com>
2013-09-05 18:58:50 -0500

    Spacing

--- c++project/Initializer.cpp
+++ c++project/Initializer.cpp
@@ -9,80 +9,78 @@
 Initializer::Initializer(int argc, const char* argv[]):
 _argc(argc),
 _argv(argv){
-	// check for arguments
-	if(_argc > 1)
-	{
-		// process arguments
-		std::set<char> _prevflags;
-		std::string _prevlong;
-		for(size_t i = 1; i < _argc; i++) {
-			std::string s = _argv[i];
-			if(s.find("--") == 0) {
-				_longflags.insert(s.substr(2));
-				_prevlong = s.substr(2);
-			} else if(s.find("-") == 0) {
-				_prevflags.clear();
-				for(size_t j = 1; j < s.length(); j++) {
-					_shortflags.insert(s[j]);
-					_prevflags.insert(s[j]);
-				}
-			} else {
-				if(_prevflags.size())
-					for(std::set<char>::iterator it = _prevflags.begin();
-							it != _prevflags.end();
-							it++)
-						_shortflagextras[*it][i] = s;
-				if(_prevlong.length())
-					_longflagextras[_prevlong][i] = s;
-				_unusedextras[i] = s;
-			}
-		}
-	}
+    // check for arguments
+    if(_argc > 1) {
+        // process arguments
+        std::set<char> _prevflags;
+        std::string _prevlong;
+        for(size_t i = 1; i < _argc; i++) {
+            std::string s = _argv[i];
+            if(s.find("--") == 0) {
+                _longflags.insert(s.substr(2));
+                _prevlong = s.substr(2);
+            } else if(s.find("-") == 0) {
+                _prevflags.clear();
+                for(size_t j = 1; j < s.length(); j++) {
+                    _shortflags.insert(s[j]);
+                    _prevflags.insert(s[j]);
+                }
+            } else {
+                if(_prevflags.size())
+                    for(std::set<char>::iterator it = _prevflags.begin();
+                            it != _prevflags.end();
+                            it++)
+                        _shortflagextras[*it][i] = s;
+                if(_prevlong.length())
+                    _longflagextras[_prevlong][i] = s;
+                _unusedextras[i] = s;
+            }
+        }
+    }
 }
 
 Initializer::~Initializer() {
-	// dtor
+    // dtor
 }
 
 bool Initializer::noargs() {
-	return _argc == 1;
+    return _argc == 1;
 }
 
 bool Initializer::flag(char c) {
-	return _shortflags.find(c) != _shortflags.end();
+    return _shortflags.find(c) != _shortflags.end();
 }
 
 bool Initializer::flag(std::string s) {
-	return _longflags.find(s) != _longflags.end();
+    return _longflags.find(s) != _longflags.end();
 }
 
 Initializer::extras_map Initializer::extras(char c) {
-	for(extras_iterator it = _shortflagextras[c].begin(); it != _shortflagextras[c].end(); it++)
-		_unusedextras.erase(it->first);
-	return _shortflagextras[c];
+    for(extras_iterator it = _shortflagextras[c].begin(); it != _shortflagextras[c].end(); it++)
+        _unusedextras.erase(it->first);
+    return _shortflagextras[c];
 }
 
 Initializer::extras_map Initializer::extras(std::string s) {
-	for(extras_iterator it = _longflagextras[s].begin(); it != _longflagextras[s].end(); it++)
-		_unusedextras.erase(it->first);
-	return _longflagextras[s];
+    for(extras_iterator it = _longflagextras[s].begin(); it != _longflagextras[s].end(); it++)
+        _unusedextras.erase(it->first);
+    return _longflagextras[s];
 }
 
 Initializer::extras_map Initializer::extras() {
-	return _unusedextras;
+    return _unusedextras;
 }
 
 std::string Initializer::reportUnused() {
-	std::stringstream ss;
-	for(extras_iterator it = _unusedextras.begin();
-			it != _unusedextras.end();
-			it++)
+    std::stringstream ss;
+    for(extras_iterator it = _unusedextras.begin();
+            it != _unusedextras.end();
+            it++)
     ss << "Ignored argument " << it->second << '\n';
-	return ss.str();
+    return ss.str();
 }
 
-std::string Initializer::pre_path(const char* c)
-{
+std::string Initializer::pre_path(const char* c) {
     std::string path(c);
     size_t path_end = path.rfind('/') + 1;
     return path.substr(0,path_end);
