e65be369937a4eb1e734eb80ed39dd60b6e7015d
Mara Kim <hacker.root@gmail.com>
2013-09-30 05:54:55 -0500

    Add Liscensing info

--- c++project/Options.h
+++ c++project/Options.h
@@ -1,5 +1,5 @@
 // Options.h
-// Mara Kim
+// Static program settings
 //
 // A singleton class which reads a
 // configuration file and provides
@@ -8,6 +8,22 @@
 // Usage:
 // Options::Instance()->get(FOO)
 //                      set(FOO)
+//
+// Copyright (C) 2013 Mara Kim
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see http://www.gnu.org/licenses/.
+
 
 #ifndef OPTIONS_H
 #define OPTIONS_H
