e61d45c8db17793a4c82bcae218074ed933c1288
Mara Kim <hacker.root@gmail.com>
2013-10-15 17:24:44 -0500

    Fix comments

--- c++project/Type.h
+++ c++project/Type.h
@@ -9,7 +9,9 @@
 // version.
 //
 // This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS // FOR A PARTICULAR PURPOSE. See the GNU General Public License for more // details.
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
 //
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see http://www.gnu.org/licenses/.
@@ -18,7 +20,7 @@
 #ifndef TYPE_H
 #define TYPE_H
 
-#include <iostream> //
+#include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
